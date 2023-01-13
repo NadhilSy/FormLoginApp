c055c21c2ec35eaf57413e66a48dffac0da44d68
Muhamad Nadhil Syarif <nadhil.dev@gmail.com>
2023-01-13 15:10:19 +0700

    second commit, move form input code to make input component reusable

---
+++ src/components/FormInput/styles.js
@@ -0,0 +1,24 @@
+import {StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        // flex:1,//kemungkinan yang bikin naik si stylesheet?
+        // justifyContent:"center",
+        // alignItems:"center"
+        margin:10
+    },
+    label:{
+        fontSize:16,
+        fontWeight:"bold"
+    },
+    textInput:{
+        height: 35,
+        width:300,
+        fontSize: 15,
+        padding: 10,
+        borderWidth: 2,
+        borderColor: "#BDC3C7"
+    }
+})
+
+export default styles
--- src/screens/Registration/Registration.js
+++ src/screens/Registration/Registration.js
@@ -1,38 +1,89 @@
 import React from "react";
 import {TextInput, View, Text, StyleSheet} from "react-native";
+import FormInput from "../../components/FormInput/FormInput";
 
 const Registration = () => {
     const [name, setName] = React.useState("");
+    const [address, setAddress] = React.useState("");
+    const [phone,setPhone] = React.useState("");
+    const [website, setWebsite] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const [role, setRole] = React.useState("");
 
     return(
         <View style={styles.container}>
-            <Text style={styles.label}>Name</Text>
-            <TextInput
-                style={styles.textInput}
+            {/*<Text style={styles.label}>Name</Text>*/}
+            {/*<TextInput*/}
+            {/*    style={styles.textInput}*/}
+            {/*    placeholder="Enter your name"*/}
+            {/*    value={name}*/}
+            {/*    onChangeText={setName}/>*/}
+            <FormInput
+                label="Name"
                 placeholder="Enter your name"
                 value={name}
-                onChangeText={setName}/>
+                onChange={setName}
+            />
+            <FormInput
+                label="Address"
+                placeholder="Enter your address"
+                value={address}
+                onChange={setAddress}
+            />
+            <FormInput
+                label="Phone"
+                placeholder="Enter your phone"
+                value={phone}
+                onChange={setPhone}
+                type="phone-pad"
+            />
+            <FormInput
+                label="Website"
+                placeholder="Enter your website"
+                value={website}
+                onChange={setWebsite}
+                type="url"
+            />
+            <FormInput
+                label="Email"
+                placeholder="Enter your email"
+                value={email}
+                onChange={setEmail}
+                type="email-address"
+            />
+            <FormInput
+                label="Password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={setPassword}
+                // type="visible-password"
+                isPassword={true}
+            />
+            <FormInput
+                label="Role"
+                placeholder="Enter your role"
+                value={role}
+                onChange={setRole}
+            />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
+    container:{
         flex:1,//kemungkinan yang bikin naik si stylesheet?
         justifyContent:"center",
-        alignItems:"center"
+        alignItems:"flex-start"
     },
-    label:{
-        fontSize:16,
-        fontWeight:"bold"
-    },
-    textInput:{
-        height: 50,
-        fontSize: 25,
-        padding: 10,
-        borderWidth: 2,
-        borderColor: "#BDC3C7"
+    title:{
+        fontSize: 24,
+        fontWeight: "bold",
+        textAlign: "center",
+        marginTop: 50
     }
 })
+
+
 
 export default Registration;
